d3773986013428cec61bf94901524a5687980eab
Sudheera Y S <59356331+DheeruYS@users.noreply.github.com>
2023-05-27 22:19:19 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define ll long long int
+const ll N = 100+5 , INF = 1e18 , MOD = 1e9+7;
+
+void solve(){
+
+}
+
+int main(){
+
+	fast;
+
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+
+	ll tc = 1;
+	// cin >> tc;
+	while(tc--) solve();
+
+	return 0;
+}
